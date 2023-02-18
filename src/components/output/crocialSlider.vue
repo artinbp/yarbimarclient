bad3ce2d659f614643825c7b66180b1e3890d26d
livicon <pluxtm@gmail.com>
2023-02-19 02:12:37 +0330

    first commit

---
+++ src/components/output/crocialSlider.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script setup>
+import store from '@/store';
+import { computed } from 'vue';
+let images = computed(()=>store.getters.getBanners)
+</script>
+
+<style scoped>
+
+</style>
